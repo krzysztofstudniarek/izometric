ba70e8d2a35ad6fa25cdf6b26253d7d04ad2ca55
Chris <krzysztofstudniarek@gmail.com>
2014-02-10 22:30:22 +0000

    Starting menu added

---
+++ src/menuObjects/Pointer.java
@@ -0,0 +1,24 @@
+package menuObjects;
+
+import Entities.AbstractEntity;
+
+public class Pointer extends AbstractEntity{
+
+	public Pointer(double x, double y, double width, double height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(int delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
